fix: Fixing major scaling and alignment issues

docs/examples/viz_kde_network.py
@@ -33,12 +33,14 @@ categories_file, edges_file, positions_file = sorted(files.keys())
 
 positions = np.loadtxt(pjoin(folder, positions_file))
 
+print(type(positions))
 
-slice = 3000
+slice = 1000
 
 positions = positions[:slice]
 cm = np.average(positions, axis = 0)
 print(positions.shape)
+print(positions)
 print(cm)
 
 radii = 1 + np.random.rand(len(positions))
@@ -48,10 +50,10 @@ radii = 1 + np.random.rand(len(positions))
 
 scene = window.Scene()
 
-scene.set_camera((-500, -500, -500), cm, (0.0, 0.0, 1.0))
+# scene.set_camera((-500, -500, -500), cm, (0.0, 0.0, 1.0))
 
 
-width, height = (600, 600)
+width, height = (1200, 1000)
 manager = window.ShowManager(
     scene,
     "demo",
@@ -61,9 +63,13 @@ manager = window.ShowManager(
 manager.initialize()
 
 em = EffectManager(manager)
-kde_actor = em.kde(positions, [40.0], "exponential", colormap = "inferno")
+kde_actor = em.kde(positions, 5.0, "gaussian", colormap = "inferno")
 
 manager.scene.add(kde_actor)
+
+markers_actor = actor.markers(positions, scales = 3.0)
+
+manager.scene.add(markers_actor)
 
 interactive = True
 

docs/examples/viz_kde_render.py
@@ -45,7 +45,7 @@ def normalize(array : np.array, min : float = 0.0, max : float = 1.0, axis : int
 ##################################################################
 # First, we need to setup the screen we will render the points to.
 
-width, height = (1200, 800) # For some reason, this keeps the scaling conserved, 
+width, height = (1200, 1000) # For some reason, this keeps the scaling conserved, 
 # but 1200 by 1000 not. Problem related to scaling. Threshold for changes to be
 # visible in this example is 850 
 
@@ -72,8 +72,8 @@ manager.initialize()
 
 n_points = 1000
 points = np.random.rand(n_points, 3)
-points = normalize(points, -5, 5)
-offset = np.array([0.0, 0.0, 0.0])
+points = normalize(points, -500, 500)
+offset = np.array([-16.7091521,  -14.52150358,  25.61470745])
 points = points + np.tile(offset, points.shape[0]).reshape(points.shape)
 
 ###################################################################
@@ -81,7 +81,7 @@ points = points + np.tile(offset, points.shape[0]).reshape(points.shape)
 # generated with ``numpy.random.rand`` as well, which are also 
 # remapped to the range of ``[0.05, 0.2]``.
 
-sigmas = normalize(np.random.rand(n_points, 1), 0.05, 0.2)
+sigmas = normalize(np.random.rand(n_points, 1), 3, 10)
 
 
 ###################################################################
@@ -107,8 +107,7 @@ kde_actor = effects.kde(points, sigmas, kernel="gaussian", colormap="inferno")
 
 manager.scene.add(kde_actor)
 
-
-markers_actor = markers(points, scales = 0.1)
+markers_actor = markers(points, scales = 3.0)
 
 manager.scene.add(markers_actor)
 

fury/actors/effect_manager.py
@@ -242,9 +242,11 @@ class EffectManager():
         textured_billboard : actor.Actor
             KDE rendering actor."""
         if not isinstance(sigmas, np.ndarray):
-            sigmas = np.array(sigmas)
+            sigmas = np.array([sigmas])
         if sigmas.shape[0] != 1 and sigmas.shape[0] != points.shape[0]:
             raise IndexError("sigmas size must be one or points size.")
+        elif sigmas.shape[0] == 1:
+            sigmas = np.repeat(sigmas[0], points.shape[0])
         if np.min(sigmas) <= 0:
             raise ValueError("sigmas can't have zero or negative values.")
 
@@ -280,13 +282,13 @@ class EffectManager():
         tex_impl = """
         // Turning screen coordinates to texture coordinates
         vec2 res_factor = vec2(res.y/res.x, 1.0);
-        vec2 renorm_tex = normalizedVertexMCVSOutput.xy*0.5 + 0.5;
+        vec2 renorm_tex = res_factor*normalizedVertexMCVSOutput.xy*0.5 + 0.5;
         vec2 tex_2 = gl_FragCoord.xy/res;
         float intensity = texture(screenTexture, tex_2).r;
 
         if(intensity<=0.0){
-            fragOutput0 = vec4(vec3(1.0), 1.0);
-            //discard;
+            //fragOutput0 = vec4(vec3(1.0), 1.0);
+            discard;
         }else{
             vec4 final_color = color_mapping(intensity, colormapTexture);
             fragOutput0 = vec4(final_color.rgb, u_opacity*final_color.a);
@@ -337,7 +339,7 @@ class EffectManager():
                            actor_scales.max(),
                            0.0]])
 
-        res = self.on_manager.size
+        res = np.array(self.on_manager.size)
 
         # Render to second billboard for color map post-processing.
         textured_billboard = billboard(
@@ -360,6 +362,9 @@ class EffectManager():
 
         def kde_callback(obj=None, event=None):
             cam_params = self.on_manager.scene.get_camera()
+            res[0], res[1]= self.on_manager.window.GetSize()
+            self.off_manager.window.SetSize(res[0], res[1])
+            shader_custom_uniforms(textured_billboard, "fragment").SetUniform2f("res", res)
             self.off_manager.scene.set_camera(*cam_params)
             self.off_manager.scene.Modified()
             shader_apply_effects(window, bill, gl_disable_depth)